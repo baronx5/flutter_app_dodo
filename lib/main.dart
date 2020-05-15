2c7fa29491e1f30815cf97c7acfbc4b867134095
hussainxgit <marksman.965@gmail.com>
2020-05-16 02:14:55 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+final List<String> images = [
+  'https://images.unsplash.com/photo-1586882829491-b81178aa622e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80',
+  'https://images.unsplash.com/photo-1586871608370-4adee64d1794?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2862&q=80',
+  'https://images.unsplash.com/photo-1586901533048-0e856dff2c0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+  'https://images.unsplash.com/photo-1586902279476-3244d8d18285?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80',
+  'https://images.unsplash.com/photo-1586943101559-4cdcf86a6f87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1556&q=80',
+  'https://images.unsplash.com/photo-1586951144438-26d4e072b891?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+  'https://images.unsplash.com/photo-1586953983027-d7508a64f4bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+];
+
+
+
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("widget.title"),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[
+                    Colors.black,
+                    Colors.black,
+                  ])
+          ),
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                width: 100,
+                height: 140,
+                color: Colors.white,
+                child: Image.network("https://i.pinimg.com/originals/fc/9f/ce/fc9fce935d8535f13a96a52e4af879c5.jpg"),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5.0),
+                //height: 300,
+
+                child: Container(
+                    child: CarouselSlider.builder(
+                      itemCount: images.length,
+                      options: CarouselOptions(
+                        autoPlay: true,
+                        aspectRatio: 2.0,
+                        enlargeCenterPage: true,
+                      ),
+                      itemBuilder: (context, index) {
+                        return Container(
+                          child: Center(
+                              child: Image.network(images[index], fit: BoxFit.cover, width: 1000)
+                          ),
+                        );
+                      },
+                    )
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5.0),
+                width: 100,
+                height: 300,
+                color: Colors.blue[300],
+                child: Text('Text3'),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5.0),
+                width: 100,
+                height: 300,
+                color: Colors.yellow[300],
+                child: Text('Text3'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
