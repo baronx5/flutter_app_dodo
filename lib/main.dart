d7f5e18cb465de778de14c325d2e620b22fd99a9
GEOLOGIST <baron1574@gmil.com>
2020-05-17 02:58:21 +0300

    new images .

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:getflutter/getflutter.dart';
 
 final List<String> imageList = [
-  "https://cdn.pixabay.com/photo/2017/12/03/18/04/christmas-balls-2995437_960_720.jpg",
+  "https://pa.namshicdn.com/product/A8/67726W/1-web-desktop-list.jpg",
   "https://cdn.pixabay.com/photo/2017/12/13/00/23/christmas-3015776_960_720.jpg",
   "https://cdn.pixabay.com/photo/2019/12/19/10/55/christmas-market-4705877_960_720.jpg",
   "https://cdn.pixabay.com/photo/2019/12/20/00/03/road-4707345_960_720.jpg",
-  "https://cdn.pixabay.com/photo/2019/12/22/04/18/x-mas-4711785__340.jpg",
-  "https://cdn.pixabay.com/photo/2016/11/22/07/09/spruce-1848543__340.jpg"
+  "https://pa.namshicdn.com/product/46/8363/v1/1-web-desktop-list.jpg",
+  "https://pa.namshicdn.com/product/A0/99584W/1-web-desktop-list.jpg"
 ];
 
 void main() {
@@ -24,9 +24,52 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('Business'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            title: Text('School'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
       backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
@@ -95,30 +138,40 @@ class _MyAppState extends State<MyApp> {
                     ],
                   ))),
               Container(
-                margin: EdgeInsets.symmetric(vertical: 5.0),
-                child: GFItemsCarousel(
-                  rowCount: 3,
-                  children: imageList.map(
-                        (url) {
+                child: GFCarousel(
+                  height: 250,
+                  viewportFraction: 0.4,
+                  items: imageList.map(
+                    (url) {
                       return Container(
-                        margin: EdgeInsets.all(5.0),
+                        color: Colors.grey[100],
+                        margin: EdgeInsets.all(8),
                         child: ClipRRect(
                           borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                          child:
-                          Image.network(url, fit: BoxFit.cover, width: 1000.0),
-
+                          child: Column(
+                            children: <Widget>[
+                              Image.network(
+                                url,
+                                fit: BoxFit.cover,
+                                width: 200.0,
+                                height: 200,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text('12 KWD'),
+                              )
+                            ],
+                          ),
                         ),
                       );
                     },
                   ).toList(),
+                  onPageChanged: (index) {
+                    setState(() {
+                      index;
+                    });
+                  },
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 5.0),
-                width: 100,
-                height: 300,
-                color: Colors.orange[300],
-                child: Text('Text3'),
               ),
             ],
           ),
