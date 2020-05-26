adc9bff4c2ac64c07468cb35229a0ee570b5f91a
GEOLOGIST <baron1574@gmil.com>
2020-05-26 15:44:31 +0300

    http2

--- lib/categories.dart
+++ lib/categories.dart
@@ -81,39 +81,62 @@ class _categoriesState extends State<categories> {
                   if (snapshot.hasData) {
                     return GridView.builder(
                         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          childAspectRatio: MediaQuery.of(context).size.height / 1500.0,
+                          childAspectRatio: MediaQuery.of(context).size.height / 1350.0,
                             crossAxisCount: 2),
                         itemCount: snapshot.data.length,
                         itemBuilder: (context, i) {
-                          return ClipRRect(
-                            borderRadius: BorderRadius.circular(40.0),
-                            child: Container(
-                              color: Colors.grey[200],
+                          return Container(
+
                               margin: EdgeInsets.all(10),
                               child: Column(
                                 mainAxisAlignment: MainAxisAlignment.start,
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: <Widget>[
-                                  Image.network(
-                                    snapshot.data[i]['imgUrl'],
-                                    fit: BoxFit.cover,
-                                    width: double.infinity,
+                                  GestureDetector(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(10.0),
+
+                                      child: Image.network(
+                                        snapshot.data[i]['imgUrl'],
+                                        fit: BoxFit.cover,
+                                        height: 250,
+                                        width: double.infinity,
+
+                                      ),
+                                    ),
+                                    onTap: (){
+                                      print(snapshot.data[i]['id']);
+                                    },
                                   ),
                                   Container(
+                                    width: double.infinity,
                                     color: Colors.white,
+
                                     child: Padding(
                                       padding: const EdgeInsets.all(5.0),
-                                      child: Text(snapshot.data[i]['name'], style: TextStyle(
-                                        fontFamily: 'DroidKufi',
-                                        fontSize: 12,
-                                      ),),
+                                      child: Row(
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                                        children: <Widget>[
+                                          Text(snapshot.data[i]["price"].toString()+ " KWD", style: TextStyle(color: Colors.lightGreen, fontSize: 12),),
+                                          Directionality(
+                                            textDirection: TextDirection.rtl,
+                                            child: Text(snapshot.data[i]['name'], style: TextStyle(
+                                              fontFamily: 'DroidKufi',
+                                              fontSize: 12,
+                                              color: Colors.black87,
+
+                                            ),),
+                                          ),
+                                        ],
+                                      ),
                                     ),
                                   ),
 
                                 ],
                               ),
-                            ),
-                          );
+                            );
                         });
                   }
                   return CircularProgressIndicator();
