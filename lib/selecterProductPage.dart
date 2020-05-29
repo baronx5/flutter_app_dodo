fb0ede99f2ff4ddf2255f9082f3ad23741587246
GEOLOGIST <baron1574@gmil.com>
2020-05-30 01:51:57 +0300

    more useful things ;]

--- lib/selecterProductPage.dart
+++ lib/selecterProductPage.dart
@@ -3,19 +3,45 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class selectedProductPage extends StatefulWidget {
+
   @override
   _selectedProductPageState createState() => _selectedProductPageState();
 }
 
 class _selectedProductPageState extends State<selectedProductPage> {
 
+
+
+
   @override
   Widget build(BuildContext context) {
-    final dynamic gerSelectedProductData = ModalRoute.of(context).settings.arguments;
+    final dynamic getSelectedProductData = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-      appBar: AppBar(
-          title: Text(gerSelectedProductData['name']),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled){
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 600.0,
+              backgroundColor: Colors.black54,
+              iconTheme: IconThemeData(
+                  color: Colors.black54
+              ),
+              floating: true,
+              pinned: false,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: CircleAvatar(
+                    backgroundColor: Colors.white70,
+                    child: Icon(Icons.arrow_upward, size: 25,)),
+                    background: Image.network(getSelectedProductData['imgUrl'], fit: BoxFit.cover,),
+              ),
+            )
+          ];
+        },
+        body: Center(
+          child: Text('Body here'),
+        ),
       ),
     );
   }
